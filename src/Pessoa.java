21f570a8beeb4e23d789905beaf55724e5db7d97
rm86924 <mont.italo5@gmail.com>
2023-06-06 12:26:03 -0300

    correcao atividade

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,10 +1,12 @@
 public class Pessoa {
 
+    // atributos
     String nome;
     String cpf;
     String rg;
     String telefone;
     String telefoneEmergencia;
+    String assinaturaTermo;
     String endereco;
 
 }
